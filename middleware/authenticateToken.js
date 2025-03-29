fd53239bcb5427bca3beac35fa41cd9ec5d3d1fe
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2025-03-29 18:58:52 +0100

    corrected try me

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,23 +1,35 @@
 import passport from "./passportConfig.js";
 
 const authenticateToken = (req, res, next) => {
-  passport.authenticate("jwt", { session: false }, (err, user, info) => {
-    if (err) {
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
+	const authHeader = req.headers["authorization"];
+	const token = authHeader && authHeader.split(" ")[1];
 
-    if (!user) {
-      if (info?.name === "TokenExpiredError") {
-        return res
-          .status(403)
-          .json({ error: "Token expired. Please log in again." });
-      }
-      return res.status(401).json({ error: "Unauthorized. Invalid token." });
-    }
+	if (token === "guest-token") {
+		req.user = {
+			email: "guest@demo.com",
+			role: "guest",
+		};
+		return next();
+	}
+	passport.authenticate("jwt", { session: false }, (err, user, info) => {
+		if (err) {
+			return res.status(500).json({ error: "Internal Server Error" });
+		}
 
-    req.user = user;
-    next();
-  })(req, res, next);
+		if (!user) {
+			if (info?.name === "TokenExpiredError") {
+				return res
+					.status(403)
+					.json({ error: "Token expired. Please log in again." });
+			}
+			return res
+				.status(401)
+				.json({ error: "Unauthorized. Invalid token." });
+		}
+
+		req.user = user;
+		next();
+	})(req, res, next);
 };
 
 export default authenticateToken;
--- middleware/authorizeRole.js
+++ middleware/authorizeRole.js
@@ -1,9 +1,10 @@
 const authorizeRole = (requiredRole) => (req, res, next) => {
-    if (req.user.role !== requiredRole) {
-      return res.status(403).json({ error: 'Forbidden: insufficient permissions.' });
-    }
-    next();
-  };
-  
-  module.exports = authorizeRole;
-  
+	if (req.user.role !== requiredRole) {
+		return res
+			.status(403)
+			.json({ error: "Forbidden: insufficient permissions." });
+	}
+	next();
+};
+
+module.exports = authorizeRole;
