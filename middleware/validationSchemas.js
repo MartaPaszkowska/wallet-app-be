e451948e68ad8a178abbf3cf0141865279682b37
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2025-04-11 17:32:54 +0200

    corrected try me

--- middleware/validationSchemas.js
+++ middleware/validationSchemas.js
@@ -1,28 +1,28 @@
 import Joi from "joi";
 
 export const registerSchema = Joi.object({
-  email: Joi.string().email().required().messages({
-    "string.email": "Email must be a valid email address",
-    "any.required": "Email is required"
-  }),
-  password: Joi.string().min(6).required().messages({
-    "string.min": "Password must be at least 8 characters long",
-    "any.required": "Password is required"
-  })
+	email: Joi.string().email().required().messages({
+		"string.email": "Email must be a valid email address",
+		"any.required": "Email is required",
+	}),
+	password: Joi.string().min(6).required().messages({
+		"string.min": "Password must be at least 6 characters long",
+		"any.required": "Password is required",
+	}),
 });
 
 export const loginSchema = Joi.object({
-  email: Joi.string().email().required().messages({
-    "string.email": "Email must be a valid email address",
-    "any.required": "Email is required"
-  }),
-  password: Joi.string().required().messages({
-    "any.required": "Password is required"
-  })
+	email: Joi.string().email().required().messages({
+		"string.email": "Email must be a valid email address",
+		"any.required": "Email is required",
+	}),
+	password: Joi.string().required().messages({
+		"any.required": "Password is required",
+	}),
 });
 
 export const refreshTokenSchema = Joi.object({
-  refreshToken: Joi.string().required().messages({
-    "any.required": "Refresh token is required"
-  })
+	refreshToken: Joi.string().required().messages({
+		"any.required": "Refresh token is required",
+	}),
 });
